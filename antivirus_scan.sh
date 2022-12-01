be5b4fefd02ed1807d69baded70391fe549d03da
Milos Vasic <i@mvasic.ru>
2022-12-01 17:24:58 +0300

    Small imporvements.

--- antivirus_scan.sh
+++ antivirus_scan.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-echo "Updating the definitions and starting the system scan" && \
-    sudo freshclam && sudo clamscan -r / && \ 
-    echo "System scan completed"
+if [ -z "$1" ]; then
+
+    WHAT="/"
+    
+else
+
+    WHAT="$1"
+fi
+
+echo "Updating the definitions and starting the system scan" &&
+    sudo freshclam &&
+    echo "We are about to scan: $WHAT" &&
+    clamscan -r "$WHAT" &&
+    echo "Scan completed"

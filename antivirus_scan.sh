4152574fd91f3d3f67144b7ed37a5696cbe95b94
Milos Vasic <i@mvasic.ru>
2022-12-01 17:34:42 +0300

    Small cleanup.

--- antivirus_scan.sh
+++ antivirus_scan.sh
@@ -9,7 +9,7 @@ else
     WHAT="$1"
 fi
 
-echo "Updating the virus definitions and starting the system scan" &&
+echo "Updating the virus definitions and starting the scan" &&
     sudo freshclam &&
     echo "We are about to scan: $WHAT" &&
     clamscan -r "$WHAT" &&

2d433c4e93e2a59507cbde6382f996d67be48077
Milos Vasic <i@mvasic.ru>
2022-12-01 16:52:41 +0300

    Messages updated.

--- antivirus_scan.sh
+++ antivirus_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Updating the AntiVirus definitions and starting the system scan" && \
+echo "Updating the definitions and starting the system scan" && \
     sudo freshclam && sudo clamscan -r / && \ 
-    echo "Scan completed"
+    echo "System scan completed"
